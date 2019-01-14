54642a4b231db22384baf6eded033aaa45255852
Icenowy Zheng <icenowy@aosc.io>
2019-01-14 23:48:05 +0800

    change test bitstream to blink
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- test_bitstream/test.v
+++ test_bitstream/test.v
@@ -1,8 +1,43 @@
+module clock_divider#(
+	parameter [ 31:0] pFactor = 1024
+	)(
+    input iwClk,
+    input iwnRst,
+    output reg orNewClk
+);
+    
+reg [31:0]rCounter;
+
+initial begin
+    rCounter <= 0;
+    orNewClk <= 0;
+end
+
+always @(posedge iwClk or negedge iwnRst) begin
+    if (!iwnRst) begin
+        rCounter <= 0;
+        orNewClk <= 0;
+    end else begin
+        rCounter <= rCounter + 1;
+        if (rCounter == pFactor / 2) begin
+            rCounter <= 0;
+            orNewClk = ~orNewClk; 
+        end
+    end
+end
+endmodule
+
 module test(
-	input key,
+	input clk,
 	output led
 );
 
-assign led = key;
+wire new_clk;
+
+clock_divider #(
+	.pFactor(8000000)
+) clkdiv(clk, 1, new_clk);
+
+assign led = new_clk;
 
 endmodule
